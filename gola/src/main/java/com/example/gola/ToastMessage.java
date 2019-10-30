26381ff53e3fb5af7916d8cbc1fb660d1d69edc2
Andrei Rat <macbook@MacBook-Pro-Mac.local>
2019-10-30 12:23:52 +0200

    added plugin

--- gola/src/main/java/com/example/gola/ToastMessage.java
+++ gola/src/main/java/com/example/gola/ToastMessage.java
@@ -3,10 +3,6 @@ package com.example.gola;
 import android.content.Context;
 import android.widget.Toast;
 
-/**
- * Created by anujgupta on 29/12/17.
- */
-
 public class ToastMessage {
 
     public static void s(Context c,String message){
